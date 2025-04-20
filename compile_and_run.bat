295f4d01bcbabdc3d5604a58e26525531c267f64
Just_N <86079830+xJust-N@users.noreply.github.com>
2025-04-20 16:23:05 +0300

    completed storage functional

--- compile_and_run.bat
+++ compile_and_run.bat
@@ -1,8 +1,7 @@
 @echo off
 setlocal
 
-javac -sourcepath "src" -d "bin" "src\Main.java"
-java -classpath "bin" "Main"
+java -classpath "out/production/second-project-xJust-N/src" "Main"
 
 pause
 rmdir /s /q "bin"
